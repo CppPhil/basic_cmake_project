7414b75c5e565d31d12bef8247c12de1cd786e0c
Phillip Schackier <CppPhil@users.noreply.github.com>
2021-09-25 18:42:24 +0200

    Use PL_ENABLE_BITMASK_OPERATORS to enable bitwise operations for our scoped enum

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,31 @@
+#include <cstdint>
+
 #include <iostream>
 
+#include <pl/bitmask.hpp>
+
 #include "main.hpp"
+
+enum class E : std::uint16_t
+{
+  A = 0b0000'0001,
+  B = 0b0000'0010,
+  C = 0b0000'0011
+};
+
+PL_ENABLE_BITMASK_OPERATORS(E)
 
 int main()
 {
   f();
+  
+  const E e{E::A | E::B};
+  
+  if (e == E::C) {
+    std::cout << "ok\n";
+  } else {
+    std::cout << "bad\n";
+  }
 }
 
 void f()
